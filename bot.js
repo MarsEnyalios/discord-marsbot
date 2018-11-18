c41d700509ebdf16fdb88d3e67fd7b3663130b02
Mars <marseb@byui.edu>
2018-11-18 15:03:09 -0700

    Revert "first commit (again)"
    This reverts commit 3e055b9110db506bc4f0d19c8e6250a72bab55cd.

---
+++ bot.js
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * FILE: bot.js
+ * @author MarsEnyalios
+ * Purpose: creates a new instance of Marsbot
+ *****************************************************************************/
+'use babel';  // idk why these are included but Gawdly uses them
+'use strict'; // in all their code, so just in case...
+
+// uses Babel & discord.js v10. https://www.npmjs.com/package.discord-graf
+const Bot = require('discord-graf').Bot; 
+
+// https://www.digitalocean.com/community/tutorials/how-to-use-winston-to-log-node-js-applications
+var logger = require('winston'); 
+
+// BOT Variables
+const about = require('./package.json'); // bot info
+const auth = require('./auth.json'); // bot token
+const prefix = '!'; // TODO: import from another file
+
+// configure logger settings... TODO: figure out what the hell this is doing
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+   colorze: true 
+}); 
+
+logger.level = 'debug';
+
+// initialize discord bot!!
+const bot = new Bot({
+   name: about.name,
+   version: about.version,
+   token: auth.token,
+   commandPrefix: prefix,
+   // an updateURL would go here
+   clientOptions: {
+      disableEveryone: true
+   }
+});
---
+++ commands/dice/max.js
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ * FILE: max.js
+ * @author Gawdly
+ * Summary: contains logic needed for calculating maximum possible roll of a 
+ *          dice expression. Comments added by MarsEnyalios
+ *****************************************************************************/
+'use babel';
+'use strict';
+
+import { Command, CommandFormatError } from 'discord-graf';
+import DiceExpression from 'dice-expression-evaluator';
+
+export default class MaxRollCommand extends Command {
+   constructor(bot) {
+      super(bot, {
+         name: 'max-roll',
+	 module: 'dice',
+         memberName: 'max',
+	 description: 'Calculates the maximum possible roll for a dice expression.',
+	 usage: 'max-roll <dice expression>',
+	 details: 'The dice expression follows the same rules as !roll, but targets (< or >) cannot be used.',
+	 examples: ['max-roll 2d20', 'max-roll 3d20 - d10 + 6']
+      });
+   }
+
+   async run(message, args) {
+      if(!args[0]) throw new CommandFormatError(this, message.guild);
+      
+      try {
+	 const maxRoll = new DiceExpression(args[0]).max();
+         return `The maximum possible roll is **${maxRoll}**.`;
+      } catch(err) {
+         return 'Invalid dice expression specified.';
+      }
+   } // ASYNC
+} // MAXROLLCOMMAND
